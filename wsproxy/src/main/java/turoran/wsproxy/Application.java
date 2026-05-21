d4cd5a97779fc5902c79e49c4e0c509a0e42b1c4
matthewjmeidt <matthewjmeidt@gmail.com>
2026-05-21 15:37:55 -0500

    Added ways to run the proxy and the client at the root level

---
+++ wsproxy/src/main/java/turoran/wsproxy/Application.java
@@ -0,0 +1,9 @@
+package turoran.wsproxy;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}

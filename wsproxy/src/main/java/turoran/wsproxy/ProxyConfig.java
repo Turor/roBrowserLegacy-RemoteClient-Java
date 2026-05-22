49f766bd509babc64c4fa5e0439fe6b5cb5aad71
matthewjmeidt <matthewjmeidt@gmail.com>
2026-05-21 21:10:28 -0500

    Data.ini update, set change for allowed targets

--- wsproxy/src/main/java/turoran/wsproxy/ProxyConfig.java
+++ wsproxy/src/main/java/turoran/wsproxy/ProxyConfig.java
@@ -4,7 +4,7 @@ import io.micronaut.context.annotation.Value;
 import jakarta.inject.Singleton;
 import lombok.Getter;
 
-import java.util.List;
+import java.util.Set;
 
 @Singleton
 @Getter
@@ -14,7 +14,7 @@ public class ProxyConfig {
     private boolean enabled;
 
     @Value("${wsproxy.allowed-targets:127.0.0.1:6900,127.0.0.1:6121,127.0.0.1:5121}")
-    private List<String> allowedTargets;
+    private Set<String> allowedTargets;
 
     public boolean isTargetAllowed(String target) {
         return allowedTargets != null && allowedTargets.contains(target);
